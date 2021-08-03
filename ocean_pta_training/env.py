5ccdd9587cc7e71bc6b4c00d157475c69d41cb21
Andrew.Landers@jda.com <Andrewlanders@tx1-1030223mbp.lan>
2021-08-03 11:56:00 -0500

    remove unused (out-dated) env code

--- ocean_pta_training/env.py
+++ ocean_pta_training/env.py
@@ -20,21 +20,3 @@ class Environment:
             for line in env_file.readlines():
                 var, value = line.strip().split("=")
                 os.environ[var] = value
-
-
-ENV_PATH: str = ".env"  # define the default location of the .env file
-
-class EnvVars:
-    CONFIG_PATH = "CONFIG_PATH"
-    PATH_TO_PORTS_FILE = "PATH_TO_PORTS_FILE"
-    PATH_TO_VESSEL_MOVEMENTS_DATA = "PATH_TO_VESSEL_MOVEMENTS_DATA"
-    PATH_TO_OD_FILE = "PATH_TO_OD_FILE"
-    PATH_TO_OUTPUT_DIRECTORY = "PATH_TO_OUTPUT_DIRECTORY"
-    PATH_TO_LOG_FILE = "PATH_TO_LOG_FILE"
-
-def set_env():
-    """
-    Set environment variables. Order of preference for getting variables:
-    1) value of sys.argv[1], if given
-    2) .env file in the default path
-    """
